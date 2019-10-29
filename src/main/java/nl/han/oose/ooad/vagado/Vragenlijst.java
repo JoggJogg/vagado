e72154729056a7e08860319a050b2aa776012081
timmerito <broertjesweening@hotmail.com>
2019-10-29 16:13:30 +0100

    kleine wijzigingen aan de vragenlijst

--- src/main/java/nl/han/oose/ooad/vagado/Vragenlijst.java
+++ src/main/java/nl/han/oose/ooad/vagado/Vragenlijst.java
@@ -19,17 +19,17 @@ public class Vragenlijst {
     private int lifetimeBest;
 
     Vraag vraag1 = new MeerkeuzeVraag(1, "Hoevaak heeft het Nederlands Elftal de wereldbeker gewonnen?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
-    Vraag vraag2 = new MeerkeuzeVraag(1, "Ja?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
-    Vraag vraag3 = new MeerkeuzeVraag(1, "Nee?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
-    Vraag vraag4 = new MeerkeuzeVraag(1, "Snbrrttttt?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
-    Vraag vraag5 = new MeerkeuzeVraag(1, "Pizza?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
-    Vraag vraag6 = new MeerkeuzeVraag(1, "Wie is Henk?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
-    Vraag vraag7 = new MeerkeuzeVraag(1, "Is een ham eten?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
-    Vraag vraag8 = new MeerkeuzeVraag(1, "Waar?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
-    Vraag vraag9 = new MeerkeuzeVraag(1, "Wat?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
-    Vraag vraag10 = new MeerkeuzeVraag(1, "Hoeveel kaas eet een kip?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
-    Vraag vraag11 = new MeerkeuzeVraag(1, "Dit is vraag 11?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
-    Vraag vraag12 = new MeerkeuzeVraag(1, "Dit is vraag 12?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
+    Vraag vraag2 = new MeerkeuzeVraag(2, "Ja?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
+    Vraag vraag3 = new MeerkeuzeVraag(3, "Nee?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
+    Vraag vraag4 = new MeerkeuzeVraag(4, "Snbrrttttt?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
+    Vraag vraag5 = new MeerkeuzeVraag(5, "Pizza?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
+    Vraag vraag6 = new MeerkeuzeVraag(6, "Wie is Henk?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
+    Vraag vraag7 = new MeerkeuzeVraag(7, "Is een ham eten?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
+    Vraag vraag8 = new MeerkeuzeVraag(8, "Waar?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
+    Vraag vraag9 = new MeerkeuzeVraag(9, "Wat?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
+    Vraag vraag10 = new MeerkeuzeVraag(10, "Hoeveel kaas eet een kip?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
+    Vraag vraag11 = new MeerkeuzeVraag(11, "Dit is vraag 11?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
+    Vraag vraag12 = new MeerkeuzeVraag(12, "Dit is vraag 12?", new Keuzemogelijkheid[] {new Keuzemogelijkheid("A", "1"), new Keuzemogelijkheid("B", "2"), new Keuzemogelijkheid("C", "3"), new Keuzemogelijkheid("D", "0")}, new Antwoord("B"));
 
     public Vragenlijst(int nummer, String naam, Thema thema) {
         this.nummer = nummer;
